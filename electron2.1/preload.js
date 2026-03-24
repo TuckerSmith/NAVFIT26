f816b499c47d3739cea1a57a0839ae4b1ca4f1f6
TuckerSmith <tuckers7401@yahoo.com>
2026-03-24 14:18:25 -0400

    removed some files

--- electron2.1/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    // This connects your 'window.api.exportPDF' to the 'export:pdf' in main.js
-    exportPDF: () => ipcRenderer.invoke('export:pdf'),
-    exportDatabase: () => ipcRenderer.invoke('db:export'),
-    saveFitrep: (data) => ipcRenderer.invoke('db:saveFitrep', data),
-});
